724677db873f9e0dcdd07717cd862dc114d986b6
isaasoemo <731217@student.dusd.net>
2025-11-04 11:40:13 -0800

    started tiles
    left off on player control scripts step

--- P4Gomez2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P4Gomez2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,8 +16,35 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+            }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 0.0f;
+        }
+        Debug.Log(horizontal);
+
+
+
+        float vertical = 0.0f;
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+            {
+            vertical = -1.0f;
+        }
+        Debug.Log(vertical);
+
+
+
+            Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
         transform.position = position;
     }
 }
