c876ced31f410b3ca69de226fb23eb718024326f
isaasoemo <731217@student.dusd.net>
2025-11-21 11:44:15 -0800

    Added Health system
    Editied script made certauin boxes walkalble finsihed Game environment and physics began health system,

--- P4Gomez2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P4Gomez2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -7,20 +7,41 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // Variables related to player character movement
     public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
+    public float speed = 3.0f;
+
+    // Variables related t0 the health system
+    public int maxHealth = 5;
+    int currentHealth;
+
+
     // Start is called before the first frame update
     void Start()
     {
-       MoveAction.Enable();
+        MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+    { move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
-        transform.position = position;
     }
-    
+
+
+    void FixedUpdate()
+    { 
+        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+
+    void ChangeHealth (int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth = amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
 }
