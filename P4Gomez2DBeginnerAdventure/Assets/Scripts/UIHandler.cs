cac8b8dd5536c9aa344d1341fac26fc5777d56d1
isaasoemo <731217@student.dusd.net>
2025-12-07 16:03:39 -0800

    Added animations
    Added 2 new scripts and added animations

--- P4Gomez2DBeginnerAdventure/Assets/Scripts/UIHandler.cs
+++ P4Gomez2DBeginnerAdventure/Assets/Scripts/UIHandler.cs
@@ -8,6 +8,11 @@ public class UIHandler : MonoBehaviour
     private VisualElement M_Healthbar;
     public static UIHandler Instance { get; private set; }
 
+    //UI dialouge window variables
+    public float displayTime = 4.0f;
+    private VisualElement m_NonPlayerDialogue;
+    private float m_TimerDisplay;
+
     // Awake is called when the script instance is being loaded (in this situation when the game scene loads)
     private void Awake()
     {
@@ -20,13 +25,38 @@ void Start()
     UIDocument uiDocument = GetComponent<UIDocument>();
         M_Healthbar = uiDocument.rootVisualElement.Q<VisualElement>("HealthBar");
     SetHealthValue(1.0f);
+
+
+        m_NonPlayerDialogue = uiDocument.rootVisualElement.Q<VisualElement>("NPCDialogue");
+        m_NonPlayerDialogue.style.display = DisplayStyle.None;
+        m_TimerDisplay = -1.0f;
     }
 
+
+
+    private void Update()
+    {
+       if (m_TimerDisplay >0 )
+        {
+            m_TimerDisplay -= Time.deltaTime;
+            if (m_TimerDisplay <0)
+            {
+                m_NonPlayerDialogue.style.display = DisplayStyle.None;
+            }
+        }
+    }
 
 
 
     public void SetHealthValue(float percentage)
     {
     M_Healthbar.style.width = Length.Percent(100 * percentage);
+    }
+
+
+    public void DisplayDialogue()
+    {
+        m_NonPlayerDialogue.style.display = DisplayStyle.Flex;
+        m_TimerDisplay = displayTime;
     }
 }
